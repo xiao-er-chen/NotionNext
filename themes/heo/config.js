0c214bc006ef648b969cd9e36b04654fb67c5fe5
xiao-er-chen <214chenfeng@163.com>
2026-06-02 16:32:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '跟踪装修',
+  HEO_HERO_TITLE_2: '全生命周期',
+  HEO_HERO_TITLE_3: '每一步都留有痕迹',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
